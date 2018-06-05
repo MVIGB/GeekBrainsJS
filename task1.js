021df45df5695c260337dda561c5d5603ed44dce
MVIGB <Vvovo@mail.ru>
2018-06-05 22:09:07 +0300

    version 2.0

--- task1.js
+++ task1.js
@@ -3,7 +3,6 @@
 var inputString = process.argv[2];
 
 if (inputString.length >= 1 & inputString.length <= 10000)
-if (inputString.replace(/[^a-zA-Z]/g, "").length != 0)
 {
 if (inputString.replace(/[^a-zA-Z]/g, "").toUpperCase() ==
     inputString.replace(/[^a-zA-Z]/g, "").toUpperCase().split("").reverse().join(""))
