3267ef3006ac6b633ae2a759706a44e27bc1f743
MVIGB <Vvovo@mail.ru>
2018-06-06 12:06:09 +0300

    version 2.1

--- task3.js
+++ task3.js
@@ -3,8 +3,10 @@
 var inputString = process.argv[2];
 
 if (inputString.length >= 1 & inputString.length <= 10000)
+{
 var outputString = inputString.replace(/[^AEIOUaeiou]/g, "") + " "
-                   + inputString.replace(/[AEIOUaeiou0-9 ]/g, "").replace(/[^a-zA-Z]/g, "") + " "
-                   + inputString.replace(/[^0-9]/g, "");
+                 + inputString.replace(/[AEIOUaeiou0-9 ]/g, "").replace(/[^a-zA-Z]/g, "") + " "
+                 + inputString.replace(/[^0-9]/g, "");
 
 process.stdout.write(outputString);
+}
