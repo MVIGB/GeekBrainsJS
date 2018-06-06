bff621937bcfbfd3ecc4a3eda022c493221a401f
MVIGB <Vvovo@mail.ru>
2018-06-06 10:57:51 +0300

    version 2.1

--- task2.js
+++ task2.js
@@ -6,7 +6,7 @@ if (inputNumber >= -10000 & inputNumber <= 10000)
 for (i = 0, j = 1, k = 0, fib = 0; i < Math.abs(inputNumber); i++, fib = j + k, j = k, k = fib)
 ;
 
-if (inputNumber < 0 & (inputNumber % 2) == 0)
+if (inputNumber < 0 & !(inputNumber % 2))
 fib *= -1;
 
 process.stdout.write(fib.toFixed(0));
--- task3.js
+++ task3.js
@@ -3,54 +3,8 @@
 var inputString = process.argv[2];
 
 if (inputString.length >= 1 & inputString.length <= 10000)
-{
-var vowels = inputString.replace(/[^AEIOUaeiou]/g, "");
-var consonants = inputString.replace(/[AEIOUaeiou0-9 ]/g, "").replace(/[^a-zA-Z]/g, "");
-var digits = inputString.replace(/[^0-9]/g, "");
-
-var code = (vowels.length == 0) ? '0' : '1';
-code += (consonants.length == 0) ? '0' : '1';
-code += (digits.length == 0) ? '0' : '1';
-
-var outputString = '';
-switch(code)
-{
-case '111':
-outputString = vowels + ' ' + consonants + ' ' + digits;
-break;
-
-case '110':
-outputString = vowels + ' ' + consonants;
-break;
-
-case '101':
-outputString = vowels + ' ' + digits;
-break;
-
-case '100':
-outputString = vowels;
-break;
-
-case '011':
-outputString = consonants + ' ' + digits;
-break;
-
-case '010':
-outputString = consonants;
-break;
-
-case '001':
-outputString = digits;
-break;
-
-case '000':
-outputString = '';
-break;
-
-default:
-outputString = 'Something is wrong!';
-break;
-}
+var outputString = inputString.replace(/[^AEIOUaeiou]/g, "") + " "
+                   + inputString.replace(/[AEIOUaeiou0-9 ]/g, "").replace(/[^a-zA-Z]/g, "") + " "
+                   + inputString.replace(/[^0-9]/g, "");
 
 process.stdout.write(outputString);
-}
