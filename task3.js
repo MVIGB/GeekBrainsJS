e81f9e61ec2847fb930795d9dcb385826919227b
MVIGB <Vvovo@mail.ru>
2018-06-10 12:53:17 +0300

    version 2.5

--- task3.js
+++ task3.js
@@ -4,11 +4,9 @@ var inputString = process.argv[2];
 
 if (inputString.length >= 1 & inputString.length <= 10000)
 {
-var vowels = inputString.replace(/[^aeiou]/gi, "");
-var consonants = inputString.replace(/[^a-z]|[aeiou]/gi, "");
+var vowels = inputString.replace(/[^aeiouy]/gi, "");
+var consonants = inputString.replace(/[^a-z]|[aeiouy]/gi, "");
 var digits = inputString.replace(/\D/g, "");
 
-var outputString = vowels + " " + consonants + " " + digits;
-
-process.stdout.write(outputString.trim().replace("  ", " "));
+process.stdout.write(vowels + " " + consonants + " " + digits);
 }
